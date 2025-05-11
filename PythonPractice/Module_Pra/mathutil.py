d2c09c053e6eb36917059f1f612e3e4f8d26607c
[Samarth Patil] <143065657+samarth3101@users.noreply.github.com>
2025-05-11 10:31:43 +0530

    Update mathutil.py

--- PythonPractice/Module_Pra/mathutil.py
+++ PythonPractice/Module_Pra/mathutil.py
@@ -10,6 +10,6 @@ def div(a,b):
     return a/b
 
 
-# here we had made the custom package named mathutil which consist simple four operation.
+# here we had made the custom package named math_util which consist simple four operation.
 '''we will import it main.py and mini.py
-import mathutil and from mathutil import add respectively'''
+import math_util and from math_util import add respectively'''
