57453bb1de52ccdc4e566d3c164c1ba440bef4d4
[Samarth Patil] <143065657+samarth3101@users.noreply.github.com>
2025-05-11 10:53:30 +0530

    Comments | Escspe sequence | print statement

---
+++ PythonPractice/Comm_Escape_Print.py
@@ -0,0 +1,16 @@
+#this is a comment
+
+'''This is a multi line comment'''
+
+#Basic Print
+print("My name is samarth")
+
+#Multiline Print
+name = "samarth"
+age = 20
+print("My name is " , name , "and I am ", age, "years old")
+
+#with sep and end
+print("A" , "B" , "C" , sep="_")
+print("Line 1" , end=" ")
+print("Line 2")
