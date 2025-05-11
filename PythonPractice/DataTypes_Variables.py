ac120eb3a16116d178bf7f1bdd741d0a5583d201
[Samarth Patil] <143065657+samarth3101@users.noreply.github.com>
2025-05-11 11:00:54 +0530

    Variables | Data Types

---
+++ PythonPractice/DataTypes_Variables.py
@@ -0,0 +1,41 @@
+#Variables
+#Container which holds the data
+
+name = "Samarth"
+age = 20
+
+print("The name is container which holds = " ,name)
+print("The age is container which holds = " ,age)
+
+#Data Types
+
+#Numeric (int, float, complex)
+age = 20
+pieValue = 3.14
+complexValue = 3.14j
+
+print(type(age))
+print(age)
+print(type(pieValue))
+print(pieValue)
+print(type(complexValue))
+print(complexValue)
+
+#Sequenced (List, Tuple)
+myList = [1,2,3,4,5]
+myTuple = (1,2,3,4,5)
+
+print(type(myList))
+print(myList)
+print(type(myTuple))
+print(myTuple)
+
+#Mapped (dict)
+mydict = {"Name" : "Samarth" , "Age" : 20}
+print(type(mydict))
+print(mydict)
+
+#Set (set)
+myset = {1,2,3,4,5,6,7,8,9}
+print(type(myset))
+print(myset)
